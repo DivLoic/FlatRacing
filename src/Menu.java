7440b70d4675761dd5455c82e34e9457b20c58e8
DivLoic <ldivad@isep.fr>
2014-06-07 19:33:06 +0200

    rest des dernier commit
    oublit

---
+++ src/Menu.java
@@ -0,0 +1,30 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.*;
+
+
+public class Menu extends FlatPanel {
+	
+	private Tunnel tunnel;
+	private ButtonManager jbm;
+	
+	@Override
+	public void buildElements() {
+		// TODO Auto-generated method stub
+		this.tunnel = new Tunnel(Parameters.SCREEN_MAX_WIDTH, -4, 25, new Color(73,73,73), new Color(73,73,73), 200);
+		this.jbm = new ButtonManager();
+	}
+	
+	@Override
+	public void personalController(Graphics2D g) {
+		// TODO Auto-generated method stub
+		tunnel.controller(g);
+		jbm.controller(g);
+	}
+
+
+	
+}
